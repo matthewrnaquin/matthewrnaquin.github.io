c40aebe80518068d64c277e73ad708989b4f521a
matthewrnaquin <matthew.r.naquin-1@ou.edu>
2023-11-26 19:45:03 -0600

    s

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 /*!
-* Start Bootstrap - Clean Blog v6.0.9 (https://startbootstrap.com/theme/clean-blog)
-* Copyright 2013-2023 Start Bootstrap
+* Matthew Naquin - Clean Blog v6.0.9 (https://startbootstrap.com/theme/clean-blog)
+* Copyright 2013-2023 Matthew Naquin
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-clean-blog/blob/master/LICENSE)
 */
 window.addEventListener('DOMContentLoaded', () => {
